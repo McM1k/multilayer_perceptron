76bc83f781ede0bad095a5c4eddcbe32516f47b1
McM1k <gallien.boudrie@gmail.com>
2020-01-06 23:17:13 +0100

    wip push

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ mod cell;
 
 extern crate rand;
 use crate::rand::Rng;
+use rand::rngs::SmallRng;
 
 fn main() {
     let data = reader::get_raw_data("resources/data.csv")
         .expect("problem while reading csv");
     let seed: [u8; 16] = [1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8];
-    let mut rng: rand::rngs::SmallRng = rand::SeedableRng::from_seed(seed);
+    let mut rng: SmallRng = rand::SeedableRng::from_seed(seed);
     println!("{:?}", rng.gen::<f64>());
 }
---
+++ src/math.rs
@@ -0,0 +1,10 @@
+pub fn cross_entropy(y: Vec<_>, p: Vec<_>) -> f64 {
+    let n = y.len();
+    let mut sum = 0.0;
+
+    for i in 0..n {
+        sum += y[i] * p[i].log10() + (1 - y[i]) * (1 - p[i]).log10();
+    }
+
+    -sum / n
+}
